c490b69601c6af354c3a55fff9784f1961025fbb
Alex <alex@sudoalex.dev>
2025-05-31 15:37:39 -0600

    feat: Add installation script for ishare2

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+ISHARE2_REPO=https://raw.githubusercontent.com/ishare2-org/ishare2-cli/main/ishare2
+ISHARE2_PATH=/usr/sbin/ishare2
+
+if wget -O $ISHARE2_PATH $ISHARE2_REPO; then
+    chmod +x $ISHARE2_PATH && ishare2
+else
+    echo "Failed to download ishare2"
+    exit 1
+fi
